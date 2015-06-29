1ef9084ef047e03de15c377001aedae1c899667e
Veljko Popovic <veljko@888.rs>
2015-06-29 16:10:52 +0200

    more tests ...

--- test_site/js/boot.js
+++
@@ -1,5 +0,0 @@
-angular.module('pb.app', ['ui.router', 'angular.lib'])
-.config(function($stateProvider, $urlRouterProvider) {
-  //your router setup goes here ... visit https://github.com/angular-ui/ui-router for more info ...
-});
-
