38e641579e47beb1d522d9fbe830c1e3cfaa4526
Andrija Petrovic <andrija.hers@gmail.com>
2015-11-04 14:38:36 +0100

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function createServicePack(execlib){
   'use strict';
+
+  var lib = execlib.lib,
+    q = lib.q,
+    d = q.defer(),
+    execSuite = execlib.execSuite,
+    libRegistry = execSuite.libRegistry;
+
+    libRegistry.register('allex_directorylib').then(
+      onDirectoryLib.bind(null, d, execlib),
+      d.reject.bind(d)
+    );
+
+    return d.promise;
+
+  /*
   var ret = require('./clientside')(execlib),
     execSuite = execlib.execSuite,
     ParentServicePack = execSuite.registry.get('.');
@@ -8,6 +23,20 @@ function createServicePack(execlib){
   ret.Service = require('./servicecreator')(execlib,ParentServicePack);
 
   return ret;
+  */
+}
+
+
+function onDirectoryLib(defer, execlib) {
+  'use strict';
+  var ret = require('./clientside')(execlib),
+    execSuite = execlib.execSuite,
+    ParentServicePack = execSuite.registry.get('.');
+
+  require('./parserregistryintroducer')(execlib);
+  ret.Service = require('./servicecreator')(execlib,ParentServicePack);
+
+  defer.resolve(ret);
 }
 
 module.exports = createServicePack;
