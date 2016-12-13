0bd9eb79b0141356fd26fa02932ec748fd7646d7
Veljko Popovic <v.s.popovic@gmail.com>
2016-12-13 15:45:39 +0100

    oops

--- browserify.js
+++ browserify.js
@@ -1,2 +1,2 @@
 ALLEX.execSuite.registry.registerClientSide('allex_directoryservice',require('./sinkmapcreator')(ALLEX, ALLEX.execSuite.registry.getClientSide('.')));
-ALLEX.execSuite.taskRegistry.registerClientSide('allex_directoryservice',require('./taskcreator')(ALLEX, ALLEX.execSuite.libRegistry.get('allex_directorylib')));
+ALLEX.execSuite.taskRegistry.register('allex_directoryservice',require('./taskcreator')(ALLEX, ALLEX.execSuite.libRegistry.get('allex_directorylib')));
--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 ALLEX.execSuite.registry.registerClientSide('allex_directoryservice',require('./sinkmapcreator')(ALLEX, ALLEX.execSuite.registry.getClientSide('.')));
-ALLEX.execSuite.taskRegistry.registerClientSide('allex_directoryservice',require('./taskcreator')(ALLEX, ALLEX.execSuite.libRegistry.get('allex_directorylib')));
+ALLEX.execSuite.taskRegistry.register('allex_directoryservice',require('./taskcreator')(ALLEX, ALLEX.execSuite.libRegistry.get('allex_directorylib')));
 
 },{"./sinkmapcreator":4,"./taskcreator":7}],2:[function(require,module,exports){
 module.exports = {
